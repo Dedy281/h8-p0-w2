341b32d7ac24f695edf9582d965487ebf65247fc
Dedy <dedypermadi28@gmail.com>
2018-05-27 00:28:31 +0700

    Exercises 6 no 2

---
+++ Exercises6_no2.js
@@ -0,0 +1,12 @@
+
+console.log('LOOPING PERTAMA');
+for(var deret = 1; deret <=20; deret++) {
+    console.log(deret + ' - I love cooding');
+  }
+
+  //===============================================
+
+console.log('LOOPING KEDUA');
+for(var deret2 = 20; deret2 >=1; deret2--) {
+    console.log(deret2 + ' - I will become fullstack developer');
+  }
