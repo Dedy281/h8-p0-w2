1f5172d74fd5bd710aee7543602906fc74756bb5
Dedy <dedypermadi28@gmail.com>
2018-05-27 00:22:15 +0700

    Exercises 4

---
+++ Exercises4.js
@@ -0,0 +1,67 @@
+var tanggal = 1
+var bulan = 12
+var tahun = 2018
+var bulanDesc;
+
+switch (bulan) {
+    case 1 : {
+        bulanDesc = 'Januari'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 2 : {
+        bulanDesc = 'Februari'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 3 : {
+        bulanDesc = 'Maret'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 4 : {
+        bulanDesc = 'April'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 5 : {
+        bulanDesc = 'Mei'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 6 : {
+        bulanDesc = 'Juni'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 7 : {
+        bulanDesc = 'Juli'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 8 : {
+        bulanDesc = 'Agustus'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 9 : {
+        bulanDesc = 'September'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 10 : {
+        bulanDesc = 'Oktober'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 11 : {
+        bulanDesc = 'November'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+    case 12 : {
+        bulanDesc = 'Desember'
+        console.log(tanggal + ' ' + bulanDesc + ' ' + tahun);
+      break;
+    }
+}
