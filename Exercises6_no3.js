6a4f5ce699cfdc0f05c2c1fd496f14703ff1c6ba
Dedy <dedypermadi28@gmail.com>
2018-05-27 00:30:14 +0700

    Exercises 6 no 3

---
+++ Exercises6_no3.js
@@ -0,0 +1,38 @@
+for(var deret = 1; deret <=100; deret++) {
+    if(deret %2===0){
+    console.log("genap");}
+      else {
+        console.log("ganjil");
+      }
+  }
+
+
+  //=================================================
+
+  for(var kelipatan3 = 1; kelipatan3 <=100; kelipatan3+=2) {
+    if(kelipatan3 %3===0){
+    console.log("3 kelipatan 3");}
+      else {
+        console.log("");
+      }
+  }
+  
+  //=================================================
+
+  for(var kelipatan6 = 1; kelipatan6 <=100; kelipatan6+=5) {
+    if(kelipatan6 %6===0){
+    console.log("6 kelipatan 6");}
+      else {
+        console.log("");
+      }
+  }
+
+  //==================================================
+
+  for(var kelipatan10 = 1; kelipatan10 <=100; kelipatan10+=9) {
+    if(kelipatan10 %10===0){
+    console.log("10 kelipatan 10");}
+      else {
+        console.log("");
+      }
+  }
