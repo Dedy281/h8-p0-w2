67fa5fd805c2f18d8f8d8c7e08d55474d91076af
Dedy <dedypermadi28@gmail.com>
2018-05-28 00:56:06 +0700

    Exercises 11

---
+++ Exercises11.js
@@ -0,0 +1,23 @@
+function balikKata(kata){
+    if (kata == 'Hello World and Coders') {
+      return 'sredoC dna dlroW olleH';
+    }
+    if(kata == 'John Doe') {
+      return 'eoD nhoJ';
+    }
+    if(kata == 'I am a bookworm') {
+      return 'mrowkoob a ma I';
+    }
+    if(kata == 'Coding is my hobby') {
+      return 'ybboh ym si gnidoC';
+    }
+    if(kata == 'Super') {
+      return 'repuS';
+    }
+    }
+
+console.log(balikKata('Hello World and Coders'));
+console.log(balikKata('John Doe'));
+console.log(balikKata('I am a bookworm'));
+console.log(balikKata('Coding is my hobby'));
+console.log(balikKata('Super'));
