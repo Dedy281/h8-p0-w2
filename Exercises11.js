f742ad846dead96ae73a2c5268c7d62b42c7a769
Dedy281 <39256056+Dedy281@users.noreply.github.com>
2018-08-29 17:17:41 +0700

    Update Exercises11.js

--- Exercises11.js
+++ Exercises11.js
@@ -1,23 +1,14 @@
-function balikKata(kata){
-    if (kata == 'Hello World and Coders') {
-      return 'sredoC dna dlroW olleH';
-    }
-    if(kata == 'John Doe') {
-      return 'eoD nhoJ';
-    }
-    if(kata == 'I am a bookworm') {
-      return 'mrowkoob a ma I';
-    }
-    if(kata == 'Coding is my hobby') {
-      return 'ybboh ym si gnidoC';
-    }
-    if(kata == 'Super') {
-      return 'repuS';
-    }
-    }
-
-console.log(balikKata('Hello World and Coders'));
-console.log(balikKata('John Doe'));
-console.log(balikKata('I am a bookworm'));
-console.log(balikKata('Coding is my hobby'));
-console.log(balikKata('Super'));
+function balikKata(kata) {
+  // you can only write your code here!
+  var balik = ''
+  for(var i = kata.length-1; i >= 0; i--){
+    balik += kata[i]
+  }
+  return balik
+}
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
